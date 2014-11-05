28bdf30bd97d2ca26f0ad7de3b1d18dfe56f9561
Ar1k <arunkmr492@gmail.com>
2014-11-05 19:39:14 +0530

    added node

---
+++ server.js
@@ -0,0 +1,40 @@
+var express =require('express');
+var express = require('express'),
+    app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+// listen for new web clients:
+server.listen(8080);
+
+     //------------------- Default Shit--------------------------------------//
+app.configure(function(){
+    app.use(express.methodOverride());
+    //app.use(express.bodyParser());
+    app.use(app.router);
+	app.use(express.json());
+    app.use(express.urlencoded());
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
+
+//------------------- Default Shit ends--------------------------------------//
+
+
+
+//Socket functions starts//
+io.sockets.on('connection', function (socket) {
+	socket.on('action', function(data){
+		socket.emit('fire', "server received: "+data);
+	})
+});
